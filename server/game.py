69920e1d863f31adbdb4782f171c0613e4d5fa11
georgesohran <soxrgeorge@gmail.com>
2024-08-20 09:45:57 +0500

    editing backend api

--- server/game.py
+++ server/game.py
@@ -1,10 +1,20 @@
 from fastapi import WebSocket, WebSocketDisconnect
+from pydantic import BaseModel
 import random
+import string
+
+
+class GameParams(BaseModel):
+  name: str | None
+  max_players: int
+  hight: int
+  width: int
+  signs2win: int
 
 
 class GameField:
-  def __init__(self, heigth: int, width: int):
-    self.fields = [[''] * width] * heigth
+  def __init__(self, game_params: GameParams):
+    self.fields = [[''] * game_params.width] * game_params.heigth
   
   def mark(self, x: int, y: int, sign: str):
     self.fields[x][y] = sign
@@ -15,15 +25,18 @@ class GameField:
 
 
 class GameRoom:
-  def __init__(self, game_params: dict):
+  def __init__(self, game_params: GameParams):
     self.players: list[WebSocket] = []
-    self.max_players: int = game_params.pop('max_players')
-    self.game_field: GameField = GameField(**game_params)
+    self.max_players: int = game_params.max_players
+    self.game_field: GameField = GameField(game_params)
 
   async def connect(self, websocket: WebSocket):
     await websocket.accept()
     if len(self.players) <= self.max_players:
       self.players.append(websocket)
+  
+  async def disconnect(self, websocket: WebSocket):
+    self.players.remove(websocket)
 
   async def send_game_data(self):
     for player in self.players:
@@ -35,8 +48,15 @@ class GameManeger:
   def __init__(self):
     self.active_games: dict[int, GameRoom] = {}
   
-  def create_game(self, game_params: dict[str, str|int|bool]):
-    game = GameRoom(game_params) 
-    self.active_games[len(self.active_games)+1] = game
+  def create_game(self, game_params: GameParams):
+    game = GameRoom(game_params)
+    if game_params.name:
+      self.active_games[game_params.name] = game
+    else:
+      name = random.choice(string.ascii_letters) * random.randint(3, 10)
+      while self.active_games[name]:
+        name = random.choice(string.ascii_letters) * random.randint(3, 10)
+      self.active_games[name] = game
+
     return len(self.active_games) + 1
   
--- server/main.py
+++ server/main.py
@@ -7,17 +7,18 @@ game_manager = GameManeger()
 
 
 @app.post('/game')
-def create_game(data):
-  game_params = data['game_params']
+def create_game(game_params: GameParams):
   id = game_manager.create_game(game_params)
   return {'detail': 'success', 'game':game_manager.active_games[id]}
 
 
 @app.websocket('game/{room_id}/{client_id}')
 async def ws(websocket: WebSocket, room_id:str, client_id:str):
-  await websocket.accept()
-  while True:
-    data = await websocket.receive_json()
-    await websocket.send({
-      
-      })
+  await game_manager.active_games[room_id].connect(websocket)
+  try:
+    while True:
+      data = await websocket.receive_json()
+      game_manager.active_games[room_id].game_field = data['game_field']
+      await websocket.send({'detail':'continue', 'game_field':data['game_field']})
+  except WebSocketDisconnect:
+     game_manager.active_games[room_id].disconnect(websocket)
